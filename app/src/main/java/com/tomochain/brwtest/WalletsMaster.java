684b2a0f79fd8fbd123d10f270789ad6aae8f62f
Lê Mạnh Niên <nienbkict@gmail.com>
2018-11-01 18:05:10 +0700

    clean cpp and add jni

---
+++ app/src/main/java/com/tomochain/brwtest/WalletsMaster.java
@@ -0,0 +1,11 @@
+package com.tomochain.brwtest;
+
+/**
+ * Created by NienLe on 01,November,2018
+ * Midsummer.
+ * Ping me at nienbkict@gmail.com
+ * Happy coding ^_^
+ */
+public class WalletsMaster {
+
+}
